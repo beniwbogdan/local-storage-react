d5a4eada64482b9a32f1ac42c14548f9a5639be6
beniwbogdan <bohdanbeniv@gmail.com>
2022-07-18 23:57:06 +0200

    realization the clear local storage function

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-type CounterType={
-    counter:number
-    setCounter:(counter:number)=>void
-}
-const Counter = (props:CounterType) => {
 
-    const setToLocalStorage=()=>{
-        localStorage.setItem('counterValue',JSON.stringify(props.counter));
+type CounterType = {
+    counter: number
+    setCounter: (counter: number) => void
+}
+const Counter = (props: CounterType) => {
+
+    const setToLocalStorage = () => {
+        localStorage.setItem('counterValue', JSON.stringify(props.counter));
+
         //`${props.counter}`
         //-or
         //props.counter.toString()
@@ -14,19 +16,29 @@ const Counter = (props:CounterType) => {
         //JSON.stringify(props.counter)
         //overall must be a string
     }
-    const getFromLocalStorage=()=>{
-         localStorage.getItem('counterValue');
+    const getFromLocalStorage = () => {
+        let valAsString = localStorage.getItem('counterValue');
+        if(valAsString){
+            let newValue = JSON.parse(valAsString);
+            props.setCounter(newValue)
+        }
 
+    }
+    const clearLocalStorage = () => {
+        localStorage.clear();
     }
 
     return (
         <div>
             {props.counter}<br/>
-            <button onClick={()=>{
-                props.setCounter(props.counter+1);
-            }}>add</button>
+            <button onClick={() => {
+                props.setCounter(props.counter + 1);
+
+            }}>add
+            </button>
             <button onClick={setToLocalStorage}>setToLocalStorage</button>
             <button onClick={getFromLocalStorage}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorage}>clearLocalStorage</button>
         </div>
     );
 };
